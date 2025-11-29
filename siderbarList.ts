7bf31f9de530f44025f703fa0d4705759a49dd73
刘引 <yin.liu@sunny-baer.com>
2025-11-29 17:35:36 +0800

    deploy

---
+++ siderbarList.ts
@@ -0,0 +1,538 @@
+export const sidebarList = {
+  "/views/关于/": [
+    {
+      "text": "关于",
+      "items": []
+    }
+  ],
+  "/views/开发/": [
+    {
+      "text": "开发",
+      "items": [
+        {
+          "text": "前端",
+          "link": "/views/开发/前端/",
+          "collapsed": false,
+          "items": [
+            {
+              "text": "axios",
+              "link": "/views/开发/前端/axios/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "css",
+              "link": "/views/开发/前端/css/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "echarts",
+              "link": "/views/开发/前端/echarts/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "echarts滚动条隐藏",
+              "link": "/views/开发/前端/echarts滚动条隐藏/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "github工作流搭建",
+              "link": "/views/开发/前端/github工作流搭建/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "html",
+              "link": "/views/开发/前端/html/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "ios编辑vitepress文档攻略",
+              "link": "/views/开发/前端/ios编辑vitepress文档攻略/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "javaScript",
+              "link": "/views/开发/前端/javaScript/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "pinia",
+              "link": "/views/开发/前端/pinia/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "print",
+              "link": "/views/开发/前端/print/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "pwa应用",
+              "link": "/views/开发/前端/pwa应用/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "td-design",
+              "link": "/views/开发/前端/td-design/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "typeScript",
+              "link": "/views/开发/前端/typeScript/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "uniapp",
+              "link": "/views/开发/前端/uniapp/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "vite",
+              "link": "/views/开发/前端/vite/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "VitePress 优化代码块样式",
+              "link": "/views/开发/前端/VitePress 优化代码块样式/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "VitePress 图片查看器插件",
+              "link": "/views/开发/前端/VitePress 图片查看器插件/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "vitepress显示git历史记录",
+              "link": "/views/开发/前端/vitepress显示git历史记录/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "vitepress路由动画效果",
+              "link": "/views/开发/前端/vitepress路由动画效果/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "vitepress阅读增强实现",
+              "link": "/views/开发/前端/vitepress阅读增强实现/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "VitePress集成评论系统",
+              "link": "/views/开发/前端/VitePress集成评论系统/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "vue",
+              "link": "/views/开发/前端/vue/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "vue2、3区别",
+              "link": "/views/开发/前端/vue2、3区别/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "事件循环",
+              "link": "/views/开发/前端/事件循环/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "作用域",
+              "link": "/views/开发/前端/作用域/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "使用 C-lodop 进行打印",
+              "link": "/views/开发/前端/使用 C-lodop 进行打印/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "兼容旧版浏览器",
+              "link": "/views/开发/前端/兼容旧版浏览器/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "原型链",
+              "link": "/views/开发/前端/原型链/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "定时器、延时器",
+              "link": "/views/开发/前端/定时器、延时器/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "捕获与冒泡",
+              "link": "/views/开发/前端/捕获与冒泡/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "暗黑切换动画",
+              "link": "/views/开发/前端/暗黑切换动画/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "检测版本更新",
+              "link": "/views/开发/前端/检测版本更新/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "浏览器硬刷新",
+              "link": "/views/开发/前端/浏览器硬刷新/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "解决vscode同名文件后缀显示copy",
+              "link": "/views/开发/前端/解决vscode同名文件后缀显示copy/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "闭包",
+              "link": "/views/开发/前端/闭包/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "防抖、节流",
+              "link": "/views/开发/前端/防抖、节流/",
+              "collapsed": false,
+              "items": []
+            }
+          ]
+        },
+        {
+          "text": "后端",
+          "link": "/views/开发/后端/",
+          "collapsed": false,
+          "items": [
+            {
+              "text": "express",
+              "link": "/views/开发/后端/express/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "linux下systemctl的使用",
+              "link": "/views/开发/后端/linux下systemctl的使用/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "mysql",
+              "link": "/views/开发/后端/mysql/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "nginx",
+              "link": "/views/开发/后端/nginx/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "ssh的使用",
+              "link": "/views/开发/后端/ssh的使用/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "ssh远程文件修改",
+              "link": "/views/开发/后端/ssh远程文件修改/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "vercel的使用",
+              "link": "/views/开发/后端/vercel的使用/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "在linux后台部署express服务",
+              "link": "/views/开发/后端/在linux后台部署express服务/",
+              "collapsed": false,
+              "items": []
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "/views/生活/": [
+    {
+      "text": "生活",
+      "items": [
+        {
+          "text": "健康",
+          "link": "/views/生活/健康/",
+          "collapsed": false,
+          "items": [
+            {
+              "text": "胆囊息肉",
+              "link": "/views/生活/健康/胆囊息肉/",
+              "collapsed": false,
+              "items": []
+            }
+          ]
+        },
+        {
+          "text": "食谱",
+          "link": "/views/生活/食谱/",
+          "collapsed": false,
+          "items": [
+            {
+              "text": "煎牛排",
+              "link": "/views/生活/食谱/煎牛排/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "番茄炒蛋",
+              "link": "/views/生活/食谱/番茄炒蛋/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "香煎三文鱼",
+              "link": "/views/生活/食谱/香煎三文鱼/",
+              "collapsed": false,
+              "items": []
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "/views/硬件/": [
+    {
+      "text": "硬件",
+      "items": [
+        {
+          "text": "CPU",
+          "link": "/views/硬件/CPU/",
+          "collapsed": false,
+          "items": [
+            {
+              "text": "CPU天梯图",
+              "link": "/views/硬件/CPU/CPU天梯图/",
+              "collapsed": false,
+              "items": []
+            }
+          ]
+        },
+        {
+          "text": "SOC",
+          "link": "/views/硬件/SOC/",
+          "collapsed": false,
+          "items": [
+            {
+              "text": "SOC天梯图",
+              "link": "/views/硬件/SOC/SOC天梯图/",
+              "collapsed": false,
+              "items": []
+            }
+          ]
+        },
+        {
+          "text": "SSD硬盘",
+          "link": "/views/硬件/SSD硬盘/",
+          "collapsed": false,
+          "items": []
+        },
+        {
+          "text": "内存条",
+          "link": "/views/硬件/内存条/",
+          "collapsed": false,
+          "items": []
+        },
+        {
+          "text": "散热器",
+          "link": "/views/硬件/散热器/",
+          "collapsed": false,
+          "items": []
+        },
+        {
+          "text": "显卡梯图",
+          "link": "/views/硬件/显卡梯图/",
+          "collapsed": false,
+          "items": []
+        },
+        {
+          "text": "显示器",
+          "link": "/views/硬件/显示器/",
+          "collapsed": false,
+          "items": []
+        },
+        {
+          "text": "电源",
+          "link": "/views/硬件/电源/",
+          "collapsed": false,
+          "items": []
+        },
+        {
+          "text": "装机",
+          "link": "/views/硬件/装机/",
+          "collapsed": false,
+          "items": []
+        },
+        {
+          "text": "键盘",
+          "link": "/views/硬件/键盘/",
+          "collapsed": false,
+          "items": []
+        },
+        {
+          "text": "鼠标",
+          "link": "/views/硬件/鼠标/",
+          "collapsed": false,
+          "items": []
+        }
+      ]
+    }
+  ],
+  "/views/软件/": [
+    {
+      "text": "软件",
+      "items": [
+        {
+          "text": "其他",
+          "link": "/views/软件/其他/",
+          "collapsed": false,
+          "items": [
+            {
+              "text": "picgo图床",
+              "link": "/views/软件/其他/picgo图床/",
+              "collapsed": false,
+              "items": []
+            }
+          ]
+        },
+        {
+          "text": "数据库应用",
+          "link": "/views/软件/数据库应用/",
+          "collapsed": false,
+          "items": [
+            {
+              "text": "mysql的安装",
+              "link": "/views/软件/数据库应用/mysql的安装/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "navicat",
+              "link": "/views/软件/数据库应用/navicat/",
+              "collapsed": false,
+              "items": []
+            }
+          ]
+        },
+        {
+          "text": "系统操作",
+          "link": "/views/软件/系统操作/",
+          "collapsed": false,
+          "items": [
+            {
+              "text": "anygo开启虚拟定位",
+              "link": "/views/软件/系统操作/anygo开启虚拟定位/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "web网页打包为安卓app",
+              "link": "/views/软件/系统操作/web网页打包为安卓app/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "windows下浏览器指定地址全屏",
+              "link": "/views/软件/系统操作/windows下浏览器指定地址全屏/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "windows应用开机自启",
+              "link": "/views/软件/系统操作/windows应用开机自启/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "windows打印支持自定义纸张大小",
+              "link": "/views/软件/系统操作/windows打印支持自定义纸张大小/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "结束端口占用",
+              "link": "/views/软件/系统操作/结束端口占用/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "解决mac软件安装包损坏",
+              "link": "/views/软件/系统操作/解决mac软件安装包损坏/",
+              "collapsed": false,
+              "items": []
+            }
+          ]
+        },
+        {
+          "text": "网络代理",
+          "link": "/views/软件/网络代理/",
+          "collapsed": false,
+          "items": [
+            {
+              "text": "clash",
+              "link": "/views/软件/网络代理/clash/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "devSidecar",
+              "link": "/views/软件/网络代理/devSidecar/",
+              "collapsed": false,
+              "items": []
+            },
+            {
+              "text": "shadowrocket",
+              "link": "/views/软件/网络代理/shadowrocket/",
+              "collapsed": false,
+              "items": []
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
